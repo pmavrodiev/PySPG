c5c36aafca11793f5921cf8bbd014e89aeed282c
Pavlin Mavrodiev <pmavrodiev@pmavrodiev.(none)>
2011-12-09 00:00:26 +0100

    new gif for the movement of the mode towards the truth

--- examples/rank/rank_detailed_investigations.R
+++ examples/rank/rank_detailed_investigations.R
@@ -13,7 +13,8 @@
   # =========================================================================================== #
 
   # =============================== init ========================================= #
-  setwd("~/Documents/scripts/rank")
+  #setwd("~/Documents/scripts/rank")
+  setwd("~/run")
 
   N=100 #number of agents
   T=500 #number of time steps
@@ -24,8 +25,8 @@
   Truth = 6
   estimates_matrix=matrix(0,N,(T+1))
   
-  data_unzipped=read.delim("Q-1_Dmax-1_eta-1.rankout.gz",header=FALSE)
-  #data_unzipped=read.delim("Q-1_Dmax-15.5_eta-25.0.rankout.gz",header=FALSE)
+  #data_unzipped=read.delim("Q-1_Dmax-1_eta-1.rankout.gz",header=FALSE)
+  data_unzipped=read.delim("Q-1_Dmax-15.5_eta-25.0.rankout.gz",header=FALSE)
   #data_unzipped=read.delim("P-1_Dmax-7.5_eta-25.0.rankout.gz",header=FALSE)
   #data_unzipped=read.delim("O-2_Dmax-10.0_eta-25.0.rankout.gz",header=FALSE)
   #data_unzipped=read.delim("O-1_Dmax-10.0_eta-25.0.rankout.gz",header=FALSE)
@@ -84,10 +85,23 @@
   plot(seq(1:(T+1)),log(abs(data_unzipped[seq(1,((T+1)*(N+1)),by=(N+1)),7]-Truth)),type="l",xlab="Time",ylab="Abs. Distance from Truth",main="Best agent currently")
 
 
-  #add the *mode* and its rank over time. need to bin the data first, else all estimates are different
-  par(new=TRUE)
-plot(seq(1:(T+1)),data_unzipped[seq(1,((T+1)*(N+1)),by=(N+1)),6],type="l",col="blue",xaxt="n",yaxt="n",xlab="",ylab="")
-  axis(4)
-  mtext("Rank",side=4,line=3)
-
-
+  #add the *mode* and its rank over time. need to bin the data first, else all estimates are different and 'mode' is undefined
+  bin_size = 0.1
+  mode_distance=NULL
+  hist_mode.fun=function() {
+     for (i in 1:(T+1)) {
+        par(mfrow=c(2,1))
+        hist_breaks=seq(min(estimates_matrix[,i]),max(estimates_matrix[,i])+bin_size,by=bin_size)
+        h=hist(estimates_matrix[,i],breaks=hist_breaks,freq=TRUE,main=paste("Time = ",i,sep=""),xlab="Estimates",ylab="Frequency")
+        h.counts = h$counts
+        h.mids = h$mids
+        Mode = h.mids[which(h.counts==max(h.counts))]
+        abline(v=Mode,col="blue")
+        abline(v=Truth)
+        mode_distance=c(mode_distance,abs(Mode-Truth))   
+        plot(1:length(mode_distance),mode_distance,type="l",col="blue")
+        ani.pause(0.001)
+    }
+  }
+  
+ saveMovie(hist_mode.fun(),movie.name="test.gif",img.name="Rplot",convert="convert",clean=TRUE)
